ca31012f1958d050092562af3936ad9349f92665
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2025-07-29 13:07:22 +0700

    migration

---
+++ CGP.Infrastructure/Migrations/20250728044603_m.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CGP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class m : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b469"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 28, 11, 46, 1, 959, DateTimeKind.Local).AddTicks(3250));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b470"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 28, 11, 46, 1, 959, DateTimeKind.Local).AddTicks(3247));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b471"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 28, 11, 46, 1, 959, DateTimeKind.Local).AddTicks(3227));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b47f"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 28, 11, 46, 1, 959, DateTimeKind.Local).AddTicks(3244));
+
+            migrationBuilder.InsertData(
+                table: "Wallet",
+                columns: new[] { "Id", "CreatedBy", "CreationDate", "DeleteBy", "DeletionDate", "IsDeleted", "ModificationBy", "ModificationDate", "Type", "User_Id" },
+                values: new object[] { new Guid("a4655df5-9aee-419c-9117-6c14a2bf2a69"), null, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), null, null, false, null, null, 2, new Guid("8b56687e-8377-4743-aac9-08dcf5c4b471") });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Wallet",
+                keyColumn: "Id",
+                keyValue: new Guid("a4655df5-9aee-419c-9117-6c14a2bf2a69"));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b469"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 26, 22, 51, 57, 876, DateTimeKind.Local).AddTicks(9130));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b470"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 26, 22, 51, 57, 876, DateTimeKind.Local).AddTicks(9124));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b471"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 26, 22, 51, 57, 876, DateTimeKind.Local).AddTicks(9085));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b47f"),
+                column: "CreationDate",
+                value: new DateTime(2025, 7, 26, 22, 51, 57, 876, DateTimeKind.Local).AddTicks(9117));
+        }
+    }
+}
