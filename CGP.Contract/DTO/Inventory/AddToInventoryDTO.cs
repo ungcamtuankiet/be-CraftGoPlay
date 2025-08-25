7101633e065b5ef9d135a619ce4611d520e69cd9
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2025-08-25 20:29:26 +0700

    Sửa chức năng thêm vật phẩm vào kho đồ

--- CGP.Contract/DTO/Inventory/AddToInventoryDTO.cs
+++ CGP.Contract/DTO/Inventory/AddToInventoryDTO.cs
@@ -11,6 +11,7 @@ namespace CGP.Contract.DTO.Inventory
         public Guid UserId { get; set; }
         public Guid? CropId { get; set; } 
         public int Quantity { get; set; }
+        public string ItemType { get; set; }
         public string InventoryType { get; set; }
         public int SlotIndex { get; set; }
     }
