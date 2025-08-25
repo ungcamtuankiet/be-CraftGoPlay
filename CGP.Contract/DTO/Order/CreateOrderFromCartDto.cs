5c898e6dd997e17abba860667bcbd1afbdfee921
nmPhuc161000 <phucnmse161000@fpt.edu.vn>
2025-08-25 23:52:38 +0700

    chỉnh dòng

--- CGP.Contract/DTO/Order/CreateOrderFromCartDto.cs
+++ CGP.Contract/DTO/Order/CreateOrderFromCartDto.cs
@@ -1,6 +1,7 @@
 ﻿using CGP.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,12 +10,24 @@ namespace CGP.Contract.DTO.Order
 {
     public class CreateOrderFromCartDto
     {
-        public Guid UserId { get; set; } 
+        [Required(ErrorMessage = "UserId is required.")]
+        public Guid UserId { get; set; }
+
+        [Required(ErrorMessage = "AddressId is required.")]
         public Guid AddressId { get; set; }
+
+        [Required(ErrorMessage = "DeliveryAmounts is required.")]
         public string DeliveryAmounts { get; set; }
+
         public string? VoucherProductCode { get; set; }
+
         public string? VoucherDeliveryCode { get; set; }
+
+        [Required(ErrorMessage = "SelectedCartItemIds is required.")]
+        [MinLength(1, ErrorMessage = "SelectedCartItemIds must contain at least one item.")]
         public List<Guid> SelectedCartItemIds { get; set; } = new();
-        public PaymentMethodEnum PaymentMethod { get; set; } 
+
+        [Required(ErrorMessage = "PaymentMethod is required.")]
+        public PaymentMethodEnum PaymentMethod { get; set; }
     }
 }
