8a9cc40b63b41eb7af577cf056a10d3770fd074a
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2025-08-26 21:56:01 +0700

    sửa bảng voucher

--- CGP.Contract/DTO/Voucher/UpdateVoucherDTO.cs
+++ CGP.Contract/DTO/Voucher/UpdateVoucherDTO.cs
@@ -1,6 +1,7 @@
 ﻿using CGP.Domain.Enums;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,19 +11,49 @@ namespace CGP.Contract.DTO.Voucher
     public class UpdateVoucherDTO
     {
         public Guid Id { get; set; }
+        [Required(ErrorMessage = "Mã giảm giá là bắt buộc.")]
+        [MaxLength(50, ErrorMessage = "Mã giảm giá phải ít hơn 50 ký tự.")]
         public string Code { get; set; }
+
+        [Required(ErrorMessage = "Tên phiếu giảm giá là bắt buộc.")]
+        [MaxLength(50, ErrorMessage = "Tên phiếu giảm giá phải ít hơn 50 ký tự.")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Mô tả phiếu giảm giá là bắt buộc.")]
+        [MaxLength(250, ErrorMessage = "Mô tả phiếu giảm giá phải ít hơn 250 ký tự.")]
         public string Description { get; set; }
+
+        [Required(ErrorMessage = "Loại phiếu giảm giá là bắt buộc.")]
         public VoucherTypeEnum Type { get; set; }
+
+        [Required(ErrorMessage = "Loại giảm giá là bắt buộc.")]
         public VoucherDiscountTypeEnum DiscountType { get; set; }
+
+        [Required(ErrorMessage = "Loại thanh toán cho phương giảm giá là bắt buộc.")]
         public PaymentMethodEnum PaymentMethod { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Giá trị nhỏ nhất để sử dụng mã giảm giá phải lớn hơn hoặc bằng 0.")]
         public double MinOrderValue { get; set; }
+
+        [Range(0, double.MaxValue, ErrorMessage = "Giá trị lớn nhất để sử dụng mã giảm giá phải lớn hơn hoặc bằng 0.")]
         public double MaxDiscountAmount { get; set; }
-        public int UsedCount { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Điểm đổi lớn hơn 0.")]
+        public double PointChangeAmount { get; set; }
+
+        [Range(0, int.MaxValue, ErrorMessage = "Số lượng phải lớn hơn 0.")]
         public int Quantity { get; set; }
+
+        [Required(ErrorMessage = "Giảm giá là bắt buộc.")]
         public double Discount { get; set; }
+
+        [Required]
         public DateTime StartDate { get; set; }
+
+        [Required]
         public DateTime EndDate { get; set; }
+
+        [Required]
         public bool IsActive { get; set; }
     }
 }
---
+++ CGP.Infrastructure/Migrations/20250826145208_fixTableVoucher.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CGP.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixTableVoucher : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "ChangeAmout",
+                table: "Voucher",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "PointChangeAmount",
+                table: "Voucher",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b471"),
+                column: "CreationDate",
+                value: new DateTime(2025, 8, 26, 21, 52, 6, 456, DateTimeKind.Local).AddTicks(7846));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b47f"),
+                column: "CreationDate",
+                value: new DateTime(2025, 8, 26, 21, 52, 6, 456, DateTimeKind.Local).AddTicks(7869));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ChangeAmout",
+                table: "Voucher");
+
+            migrationBuilder.DropColumn(
+                name: "PointChangeAmount",
+                table: "Voucher");
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b471"),
+                column: "CreationDate",
+                value: new DateTime(2025, 8, 26, 16, 5, 5, 759, DateTimeKind.Local).AddTicks(7575));
+
+            migrationBuilder.UpdateData(
+                table: "User",
+                keyColumn: "Id",
+                keyValue: new Guid("8b56687e-8377-4743-aac9-08dcf5c4b47f"),
+                column: "CreationDate",
+                value: new DateTime(2025, 8, 26, 16, 5, 5, 759, DateTimeKind.Local).AddTicks(7598));
+        }
+    }
+}
