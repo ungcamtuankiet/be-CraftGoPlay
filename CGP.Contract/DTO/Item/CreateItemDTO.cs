6ac404de2511f837b8b0edcbdd5e2eb4a9e87fae
ungcamtuankiet <ungcamtuankiet94@gmai.com>
2025-08-26 21:42:21 +0700

    sửa chức năng tạo Item

--- CGP.Contract/DTO/Item/CreateItemDTO.cs
+++ CGP.Contract/DTO/Item/CreateItemDTO.cs
@@ -9,6 +9,7 @@ namespace CGP.Contract.DTO.Item
 {
     public class CreateItemDTO
     {
+        public Guid Id { get; set; }
         public string NameItem { get; set; }
         public string Description { get; set; }
         public ItemTypeEnum ItemType { get; set; }
